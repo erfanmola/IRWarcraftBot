f106758b9bb0f504eedd71c1fced69e24208aeda
Erfan Mola <hi@erfanxd.ir>
2022-06-12 22:47:47 +0000

    Update

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set("log_errors", 1);
+ini_set("error_log", __DIR__ . "/errors.log");
+
+defined('ABSPATH') or die("Well, That's smart guess, But sry I'm smarter than you (=");
+
+require_once __DIR__ . "/information.php";
+require_once __DIR__ . "/dpxlib-beta.php";
---
+++ information-sample.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('ABSPATH') or die("Well, That's smart guess, But sry I'm smarter than you (=");
+
+date_default_timezone_set('Asia/Tehran');
+
+$token = "BOT_TOKEN";
+$botid = "BOT_ID";
+
+define('dpx_token', $token);
+define('dpx_async', true);
+define('dpx_bot_api_server', 'http://api.telegram.org');
+define('dpx_admin', 111999636);
